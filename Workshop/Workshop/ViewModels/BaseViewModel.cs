b425dc20feef9167399112950d4eadc62ee2e436
salvatore <salvatore.sorrentino@live.com>
2022-06-17 11:17:52 +0200

    visualizzazione dati

---
+++ Workshop/Workshop/ViewModels/BaseViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Workshop.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private bool isLoading;
+        private string title;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public bool IsLoading { 
+            get { return isLoading; }
+            set
+            {
+                if (isLoading == value) return;
+                isLoading = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Title { 
+            get { return title; }
+            set
+            {
+                if(title == value) return;
+                title = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+}
---
+++ Workshop/Workshop/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Workshop.Models;
+using Newtonsoft.Json;
+using Xamarin.Forms;
+
+namespace Workshop.ViewModels
+{
+    public class MainPageViewModel: BaseViewModel
+    {
+        public ObservableCollection<Monkey> Monkeys { get; }
+        public Command GetMonkeysCommand { get; }
+
+        public MainPageViewModel()
+        {
+            Title = "Monkeys App";
+            Monkeys = new ObservableCollection<Monkey>();
+            GetMonkeysCommand = new Command(async () => await GetMonkeysAsync());
+        }
+
+
+
+
+        async Task GetMonkeysAsync()
+        {
+            if (IsLoading) return;
+
+            try
+            {
+                IsLoading = true;
+
+                var httpClient = new HttpClient();
+                var json = await httpClient.GetStringAsync("https://montemagno.com/monkeys.json");
+                var monkeys = JsonConvert.DeserializeObject<List<Monkey>>(json);
+                Monkeys.Clear();
+                foreach (var monkey in monkeys)
+                {
+                    Monkeys.Add(monkey);
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                await Application.Current.MainPage.DisplayAlert("Errore!!!", ex.Message, "OK");
+            }
+            finally
+            {
+                IsLoading = false;
+            }
+
+
+        }
+
+    }
+}
